02c6f5185b331a0b301b54ad13f52f6ba7b6ad3e
nanda <nandadatta45@gmail.com>
2024-08-09 23:22:25 +0530

    complete weather data api with temperature and sky conditions

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import streamlit as st
 import plotly.express as px
 from backend import get_data
+from datetime import datetime
 
 # Setting the title
 st.title("Weather Forecast fot the Next Day's")
@@ -19,8 +20,56 @@ option = st.selectbox("Select data to view",
 # sub header to display the header
 st.subheader(f"{option} for the next {days} days in {place}")
 
+if place:
+    try:
+        # Get the temperature/sky data
+        filtered_data = get_data(place, days)
 
-# plotting area
-d, t = get_data(place, days, kind)
-figure = px.line(x=d, y=t, labels={"x": "Date", "y": "Temperature (C)"})
-st.plotly_chart(figure)
+        # temperature/sky plotting area
+        if option == "Temperature":
+
+            # Getting temperature data
+            temperatures = [dict['main']['temp'] / 10 for dict in filtered_data]
+
+            # Getting dates
+            dates = [dict["dt_txt"] for dict in filtered_data]
+
+            # plotting the data
+            figure = px.line(x=dates, y=temperatures, labels={"x": "Date", "y": "Temperature (C)"})
+            st.plotly_chart(figure)
+
+        if option == "Sky":
+            # storing the images path
+            images = {"Clear": "images/clear.png", "Clouds": "images/cloud.png", "Rain": "images/rain.png",
+                      "Snow": "images/snow.png"}
+
+            # Getting sky conditions data
+            sky_conditions = [dict['weather'][0]['main'] for dict in filtered_data]
+
+            # Getting dates
+            dates = [dict["dt_txt"] for dict in filtered_data]
+
+            # generating the image paths according to the sky conditions
+            image_paths = [images[condition] for condition in sky_conditions]
+
+            # setting 4 images per row
+            max_images_per_row = 4
+
+            # iterating over the length of image paths and images per row to skip
+            for i in range(0, len(image_paths), max_images_per_row):
+
+                # creating columns to display the images
+                cols = st.columns(min(max_images_per_row, len(sky_conditions)-i))
+
+                # iterating columns, image_path and for date to display
+                for col, image_path, date_str in zip(cols, image_paths[i:i + max_images_per_row], dates[i:i + max_images_per_row]):
+                    print(image_paths)
+                    with col:
+                        print(image_path)
+                        date_obj = datetime.strptime(date_str, "%Y-%m-%d %H:%M:%S")
+                        formatted_date = date_obj.strftime("%a, %b, %d")
+                        st.image(image_path, width=150)
+                        st.write(f"<div style='text-align: center;'>{formatted_date}</div>", unsafe_allow_html=True)
+    except KeyError:
+        st.write("Place does not exist!!.")
+
